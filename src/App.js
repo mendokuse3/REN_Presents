33bb3f44c8b405963df9df7eefa7211c762abeb9
Leo <hamlap6@gmail.com>
2021-02-25 22:40:34 -0800

    hooked up banner to pull from contenful

--- src/App.js
+++ src/App.js
@@ -4,18 +4,26 @@ import Episodes from './components/Episodes';
 import About from './components/About';
 import Contact from './components/Contact';
 
-import { getHomePage } from './Queries/index';
+import { getEpisodes, getBannerLogo, getHomePage } from './Queries/index';
 
 class App extends React.Component {
   state = {
     currentTab: 'episodes',
-    data: {}
+    episodes: {},
+    bannerLogo: {},
+    aboutAndContact: {}
   }
 
   async componentDidMount(){
-    const test = await getHomePage();
-    this.setState({data: test})
-    console.log(test)
+    const episodeData = await getEpisodes();
+    const bannerLogoData = await getBannerLogo();
+    const homePageData = await getHomePage();
+    this.setState({
+      episodes: episodeData,
+      bannerLogo: bannerLogoData,
+      aboutAndContact: homePageData
+    })
+    // console.log(episodeData)
   }
 
   setCurrentTab = (tab) => {
@@ -23,21 +31,25 @@ class App extends React.Component {
   }
 
   render(){
-    return (
-      <>
-        <NavBar setCurrentTab={this.setCurrentTab} />
-        <div className='bannerimage'>
-          <img src='https://images.unsplash.com/photo-1519638399535-1b036603ac77?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=3578&q=80' alt='placeholder banner' />
-        </div>
-        <h1>REN Presents: The Beacon</h1>
-        <div className="grid grid-cols-2">
-          {this.state.currentTab === 'episodes' && <Episodes />}
-        </div>
-
-        {this.state.currentTab === 'about' && <About description='asdfasdf' />}
-        {this.state.currentTab === 'contact' && <Contact />}
-      </>
-    )
+    if(Object.entries(this.state.bannerLogo).length > 0){
+      console.log(this.state.episodes)
+      return (
+        <>
+          <NavBar setCurrentTab={this.setCurrentTab} />
+          <div className='bannerimage'>
+            <img src={this.state.bannerLogo.fields.bannerImage.fields.file.url} alt={this.state.bannerLogo.fields.bannerImage.fields.file.fileName} />
+          </div>
+          <h1>REN Presents: The Beacon</h1>
+          <div className="grid grid-cols-2">
+            {this.state.currentTab === 'episodes' && <Episodes />}
+          </div>
+  
+          {this.state.currentTab === 'about' && <About description='asdfasdf' />}
+          {this.state.currentTab === 'contact' && <Contact />}
+        </>
+      )
+    }
+    return <div>Loading...</div>
   }
 }
 
--- src/Queries/index.js
+++ src/Queries/index.js
@@ -5,12 +5,22 @@ export const contentfulClient = new ContentfulClient({
     space: process.env.REACT_APP_SPACE_ID
 });
 
-export const getAboutPage = async () => {
-    const data = await contentfulClient.getEntry('7aDW8T03cu81ZmlQGlWYO0')
-    return data
-};
+// export const getAboutPage = async () => {
+//     const data = await contentfulClient.getEntry('7aDW8T03cu81ZmlQGlWYO0')
+//     return data
+// };
 
 export const getHomePage = async () => {
     const data = await contentfulClient.getEntry('7uNnbAhDq1rKg0u1oo9H2s')
+    return data
+};
+
+export const getEpisodes = async () => {
+    const data = await contentfulClient.getEntry('MIHg86Xc2KrPxZ43Hal0X')
+    return data
+};
+
+export const getBannerLogo = async () => {
+    const data = await contentfulClient.getEntry('2h4aI71QUeRrx7MTQyYAOm')
     return data
 };
